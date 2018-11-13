42a790ac538cf8ca1e953f55ed9278b3327ce8f3
guffenix <guffenix@localhost>
2018-11-12 22:49:04 -0500

    Actualizacion Pago Internet

--- src/main/java/ec/edu/espe/arqui/facade/PagoFacade.java
+++ src/main/java/ec/edu/espe/arqui/facade/PagoFacade.java
@@ -32,7 +32,7 @@ public class PagoFacade extends AbstractFacade<Pago> {
     }
 
     public Pago obtenerPagoByCliente(String _identificacion) {
-        BigDecimal serId = BigDecimal.valueOf(4);
+        BigDecimal serId = BigDecimal.valueOf(25);
         BigInteger pagEstado = BigInteger.valueOf(1);
         em.getEntityManagerFactory().getCache().evictAll();
         return (Pago) getEntityManager()
@@ -60,6 +60,22 @@ public class PagoFacade extends AbstractFacade<Pago> {
 
     public Pago obtenerPago(String _identificacion) {
         BigDecimal serId = BigDecimal.valueOf(22);
+        BigInteger pagEstado = BigInteger.valueOf(1);
+        em.getEntityManagerFactory().getCache().evictAll();
+        List<Pago> listaR = getEntityManager()
+                .createQuery("Select p from Pago p where p.serId.serId = ?1 AND p.cliIdentificacion.cliIdentificacion = ?2 AND p.pagEstado = ?3")
+                .setParameter(1, serId)
+                .setParameter(2, _identificacion)
+                .setParameter(3, pagEstado).getResultList();
+        if (listaR != null && !listaR.isEmpty()) {
+            return listaR.get(0);
+        } else {
+            return null;
+        }
+    }
+    
+    public Pago obtenerPagoInternet(String _identificacion) {
+        BigDecimal serId = BigDecimal.valueOf(26);
         BigInteger pagEstado = BigInteger.valueOf(1);
         em.getEntityManagerFactory().getCache().evictAll();
         List<Pago> listaR = getEntityManager()
--- src/main/java/ec/edu/espe/arqui/servicio/PagoMulta.java
+++ src/main/java/ec/edu/espe/arqui/servicio/PagoMulta.java
@@ -8,10 +8,14 @@ import ec.edu.espe.arqui.facade.ClienteFacade;
 import ec.edu.espe.arqui.facade.PagoFacade;
 import ec.edu.espe.arqui.facade.VehiculoFacade;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -39,10 +43,26 @@ public class PagoMulta implements Serializable {
 
     private Cliente cliente;
     private Vehiculo vehiculo;
-    private Pago pago;
     private String identificacionCliente;
     private String nameUsuario;
     private String idUsuario;
+
+    // caja
+    private Pago nuevoPago;
+    private BigDecimal contadorCentavo;
+    private BigDecimal contadorCincoCentavos;
+    private BigDecimal contadorDiezCentavos;
+    private BigDecimal contadorVcincoCentavos;
+    private BigDecimal contadorCincuentaCentavos;
+    private BigDecimal contadorDolarCentavos;
+
+    private BigDecimal contadorUnDolar;
+    private BigDecimal contadorCincoDolares;
+    private BigDecimal contadorDiezDolares;
+    private BigDecimal contadorVeinteDolares;
+
+    private BigDecimal valorReceptado;
+    private BigDecimal cambio;
 
     public PagoMulta() {
     }
@@ -60,17 +80,67 @@ public class PagoMulta implements Serializable {
         if (this.vehiculo != null) {
             this.cliente = this.clienteFacade.obtenerClienteByPlaca(this.vehiculo.getViPlaca());
             if (this.cliente != null) {
-                this.pago = this.pagoFacade.obtenerPagoByCliente(this.cliente.getCliIdentificacion());
+                this.nuevoPago = this.pagoFacade.obtenerPagoByCliente(this.cliente.getCliIdentificacion());
             }
         } else {
             this.cliente = new Cliente();
         }
     }
 
+    public void actualizarEstado() {
+        BigDecimal contadorCentavoAux = ((contadorCentavo != null) ? contadorCentavo : BigDecimal.ZERO).multiply(new BigDecimal("0.01"));
+        BigDecimal contadorCincoCentavosAux = ((contadorCincoCentavos != null) ? contadorCincoCentavos : BigDecimal.ZERO).multiply(new BigDecimal("0.05"));;
+        BigDecimal contadorDiezCentavosAux = ((contadorDiezCentavos != null) ? contadorDiezCentavos : BigDecimal.ZERO).multiply(new BigDecimal("0.10"));;
+        BigDecimal contadorVcincoCentavosAux = ((contadorVcincoCentavos != null) ? contadorVcincoCentavos : BigDecimal.ZERO).multiply(new BigDecimal("0.25"));;
+        BigDecimal contadorCincuentaCentavosAux = ((contadorCincuentaCentavos != null) ? contadorCincuentaCentavos : BigDecimal.ZERO).multiply(new BigDecimal("0.50"));;
+        BigDecimal contadorDolarCentavosAux = ((contadorDolarCentavos != null) ? contadorDolarCentavos : BigDecimal.ZERO).multiply(new BigDecimal("1"));;
+        BigDecimal contadorUnDolarAux = ((contadorUnDolar != null) ? contadorUnDolar : BigDecimal.ZERO).multiply(new BigDecimal("1"));;
+        BigDecimal contadorCincoDolaresAux = ((contadorCincoDolares != null) ? contadorCincoDolares : BigDecimal.ZERO).multiply(new BigDecimal("5"));;
+        BigDecimal contadorDiezDolaresAux = ((contadorDiezDolares != null) ? contadorDiezDolares : BigDecimal.ZERO).multiply(new BigDecimal("10"));;
+        BigDecimal contadorVeinteDolaresAux = ((contadorVeinteDolares != null) ? contadorVeinteDolares : BigDecimal.ZERO).multiply(new BigDecimal("20"));;
+        valorReceptado = contadorCentavoAux.add(contadorCincoCentavosAux).add(contadorDiezCentavosAux).add(contadorVcincoCentavosAux).add(contadorCincuentaCentavosAux).add(contadorDolarCentavosAux).add(contadorUnDolarAux).add(contadorCincoDolaresAux).add(contadorDiezDolaresAux).add(contadorVeinteDolaresAux).setScale(2, RoundingMode.HALF_UP);
+        cambio = valorReceptado.subtract(nuevoPago.getPagValor());
+    }
+
+    public void registrarPago() {
+        if (cambio.compareTo(BigDecimal.ZERO) >= 0) {
+            nuevoPago.setPagEstado(new BigInteger("2"));
+            pagoFacade.actualizarPago(nuevoPago);
+            cliente = null;
+            identificacionCliente = null;
+            //contraPartidaIngresada = null;
+            nuevoPago = null;
+            valorReceptado = null;
+            cambio = null;
+
+            contadorCentavo = null;
+            contadorCincoCentavos = null;
+            contadorDiezCentavos = null;
+            contadorVcincoCentavos = null;
+            contadorCincuentaCentavos = null;
+            contadorDolarCentavos = null;
+            contadorUnDolar = null;
+            contadorCincoDolares = null;
+            contadorDiezDolares = null;
+            contadorVeinteDolares = null;
+            valorReceptado = null;
+            cambio = null;
+        } else {
+            mostrarMensaje("debe ingresar un numero mayor", false);
+            System.out.println("debe ingresar un numero mayor");
+        }
+
+    }
+
+    private void mostrarMensaje(String _mensaje, boolean _esInformativo) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(_esInformativo ? FacesMessage.SEVERITY_INFO : FacesMessage.SEVERITY_ERROR, _mensaje, null));
+    }
+
     private void getUsuario(HttpServletRequest request) {
         HttpSession session = request.getSession();
         nameUsuario = (String) session.getAttribute("usuario");
-         idUsuario = (String) session.getAttribute("idUsuario");
+        idUsuario = (String) session.getAttribute("idUsuario");
     }
 
     public PagoMulta(Tipomoneda tipoMoneda, List<Tipomoneda> listaMoneda) {
@@ -136,14 +206,6 @@ public class PagoMulta implements Serializable {
         this.vehiculo = vehiculo;
     }
 
-    public Pago getPago() {
-        return pago;
-    }
-
-    public void setPago(Pago pago) {
-        this.pago = pago;
-    }
-
     public String getNameUsuario() {
         return nameUsuario;
     }
@@ -158,6 +220,134 @@ public class PagoMulta implements Serializable {
 
     public void setIdUsuario(String idUsuario) {
         this.idUsuario = idUsuario;
+    }
+
+    public VehiculoFacade getVehiculoFacade() {
+        return vehiculoFacade;
+    }
+
+    public void setVehiculoFacade(VehiculoFacade vehiculoFacade) {
+        this.vehiculoFacade = vehiculoFacade;
+    }
+
+    public ClienteFacade getClienteFacade() {
+        return clienteFacade;
+    }
+
+    public void setClienteFacade(ClienteFacade clienteFacade) {
+        this.clienteFacade = clienteFacade;
+    }
+
+    public PagoFacade getPagoFacade() {
+        return pagoFacade;
+    }
+
+    public void setPagoFacade(PagoFacade pagoFacade) {
+        this.pagoFacade = pagoFacade;
+    }
+
+    public BigDecimal getContadorCentavo() {
+        return contadorCentavo;
+    }
+
+    public void setContadorCentavo(BigDecimal contadorCentavo) {
+        this.contadorCentavo = contadorCentavo;
+    }
+
+    public BigDecimal getContadorCincoCentavos() {
+        return contadorCincoCentavos;
+    }
+
+    public void setContadorCincoCentavos(BigDecimal contadorCincoCentavos) {
+        this.contadorCincoCentavos = contadorCincoCentavos;
+    }
+
+    public BigDecimal getContadorDiezCentavos() {
+        return contadorDiezCentavos;
+    }
+
+    public void setContadorDiezCentavos(BigDecimal contadorDiezCentavos) {
+        this.contadorDiezCentavos = contadorDiezCentavos;
+    }
+
+    public BigDecimal getContadorVcincoCentavos() {
+        return contadorVcincoCentavos;
+    }
+
+    public void setContadorVcincoCentavos(BigDecimal contadorVcincoCentavos) {
+        this.contadorVcincoCentavos = contadorVcincoCentavos;
+    }
+
+    public BigDecimal getContadorCincuentaCentavos() {
+        return contadorCincuentaCentavos;
+    }
+
+    public void setContadorCincuentaCentavos(BigDecimal contadorCincuentaCentavos) {
+        this.contadorCincuentaCentavos = contadorCincuentaCentavos;
+    }
+
+    public BigDecimal getContadorDolarCentavos() {
+        return contadorDolarCentavos;
+    }
+
+    public void setContadorDolarCentavos(BigDecimal contadorDolarCentavos) {
+        this.contadorDolarCentavos = contadorDolarCentavos;
+    }
+
+    public BigDecimal getContadorUnDolar() {
+        return contadorUnDolar;
+    }
+
+    public void setContadorUnDolar(BigDecimal contadorUnDolar) {
+        this.contadorUnDolar = contadorUnDolar;
+    }
+
+    public BigDecimal getContadorCincoDolares() {
+        return contadorCincoDolares;
+    }
+
+    public void setContadorCincoDolares(BigDecimal contadorCincoDolares) {
+        this.contadorCincoDolares = contadorCincoDolares;
+    }
+
+    public BigDecimal getContadorDiezDolares() {
+        return contadorDiezDolares;
+    }
+
+    public void setContadorDiezDolares(BigDecimal contadorDiezDolares) {
+        this.contadorDiezDolares = contadorDiezDolares;
+    }
+
+    public BigDecimal getContadorVeinteDolares() {
+        return contadorVeinteDolares;
+    }
+
+    public void setContadorVeinteDolares(BigDecimal contadorVeinteDolares) {
+        this.contadorVeinteDolares = contadorVeinteDolares;
+    }
+
+    public BigDecimal getValorReceptado() {
+        return valorReceptado;
+    }
+
+    public void setValorReceptado(BigDecimal valorReceptado) {
+        this.valorReceptado = valorReceptado;
+    }
+
+    public BigDecimal getCambio() {
+        return cambio;
+    }
+
+    public void setCambio(BigDecimal cambio) {
+        this.cambio = cambio;
+    }
+
+    public Pago getNuevoPago() {
+        return nuevoPago;
+    }
+
+    public void setNuevoPago(Pago nuevoPago) {
+        this.nuevoPago = nuevoPago;
     }
 
 }
